f2f538041044179a12148ac57356b61849c2dd94
doppg <som0@meta.ua>
2020-06-06 16:23:11 +0300

    test

--- config/db_connect.php
+++ config/db_connect.php
@@ -2,8 +2,15 @@
 <?php
     //MySQLi or PDO approaches exist for now wewill use MySQLi, but experineced user should learn PDO
 
+    $dbhost = 'eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306';
+    $dbuser = 'crjbrew5zx2e5ofj';
+    $dbpass = 'tff3cduhu03wgx8k';
+    $dbname = 'phptest';
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to '$dbhost'");
+    mysqli_select_db($conn, $dbname) or die("Could not open the database '$dbname'");
+
     // create connection
-    $conn = mysqli_connect('localhost', 'ole', '1111', 'pd');
+    //$conn = mysqli_connect('localhost', 'ole', '1111', 'pd');
 
     //check connection
     if(!$conn) {  echo 'Connection error: '.mysqli_connect_error(); }
