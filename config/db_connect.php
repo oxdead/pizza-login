3598b0961e143b11f522eaee73e1bae5ee038d81
username <som0@meta.ua>
2020-09-08 19:51:24 +0300

    db accounts

--- config/db_connect.php
+++ config/db_connect.php
@@ -2,10 +2,10 @@
 <?php
     //MySQLi or PDO approaches exist for now wewill use MySQLi, but experineced user should learn PDO
 
-    // $dbhost = 'eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306';
-    // $dbuser = 'crjbrew5zx2e5ofj';
-    // $dbpass = 'tff3cduhu03wgx8k';
-    // $dbname = 'j7pi3mr52z5cw6c7';
+    $dbhost = 'eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306';
+    $dbuser = 'crjbrew5zx2e5ofj';
+    $dbpass = 'tff3cduhu03wgx8k';
+    $dbname = 'j7pi3mr52z5cw6c7';
 
     //$dburl = getenv('JAWSDB_URL');
     //$dbparts = parse_url($dburl);
@@ -15,10 +15,10 @@
     //$dbpass = $dbparts['pass'];
     //$dbname = ltrim($dbparts['path'],'/');
     
-    $dbhost = '127.0.0.1';
-    $dbuser = 'oxdead';
-    $dbpass = '1111';
-    $dbname = 'test_my';
+    //$dbhost = '127.0.0.1';
+    //$dbuser = 'oxdead';
+    //$dbpass = '1111';
+    //$dbname = 'test_my';
 
 
 
